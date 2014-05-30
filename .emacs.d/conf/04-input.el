7f0651f34d71457c873104e5d1291ff741c1c596
en30 <en30.git@gmail.com>
2014-05-30 17:44:50 +0900

    changes expand-region keys to minor keys

--- .emacs.d/conf/04-input.el
+++ .emacs.d/conf/04-input.el
@@ -17,5 +17,5 @@
 
 ;; expand-region
 (require 'expand-region)
-(global-set-key (kbd "C-@") 'er/expand-region)
-(global-set-key (kbd "C-M-@") 'er/contract-region)
+(global-set-key (kbd "C-c C-SPC") 'er/expand-region)
+(global-set-key (kbd "C-c C-M-SPC") 'er/contract-region)
