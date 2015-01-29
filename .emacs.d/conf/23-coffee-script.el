71eea2b88ed20df7d148545c1e706601174bd37b
en30 <en30.git@gmail.com>
2015-01-30 01:48:37 +0900

    emacs: shorten too long config

--- .emacs.d/conf/23-coffee-script.el
+++ .emacs.d/conf/23-coffee-script.el
@@ -1,12 +1,4 @@
 ;; coffee-mode
 (require 'coffee-mode)
 
-;; デフォルトだとインデントが８スペースなので、２スペースに変更
-;; http://qiita.com/items/a8d96ae95a1838500e37
-(defun coffee-custom ()
-  "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2)
- (setq coffee-tab-width 2))
-(add-hook 'coffee-mode-hook
-  '(lambda() (coffee-custom)))
-
+(custom-set-variables '(coffee-tab-width 2))
