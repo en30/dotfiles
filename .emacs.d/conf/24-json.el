26d373b563d5f4e8de308a56f48a9763942e9205
en30 <en30.git@gmail.com>
2015-03-17 16:12:52 +0900

    emacs: makes indent level 2 in json-mode

--- .emacs.d/conf/24-json.el
+++ .emacs.d/conf/24-json.el
@@ -1,3 +1,7 @@
 ;; json-mode
 (require 'json-mode)
 (add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-variable-buffer-local 'js-indent-level)
+            (setq js-indent-level 2)))
