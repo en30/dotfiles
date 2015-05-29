b2281997934f3aaa8925825a8b88d32b6028a72b
en30 <en30.git@gmail.com>
2015-05-30 00:18:25 +0900

    emacs: adds yasnippet

--- .emacs.d/conf/03-input.el
+++ .emacs.d/conf/03-input.el
@@ -53,3 +53,25 @@
           (lambda ()
             (define-key ido-completion-map (kbd "C-f") 'ido-next-match)
             (define-key ido-completion-map (kbd "C-b")   'ido-prev-match)))
+
+;; yasnippet
+(add-to-list 'load-path
+             "~/.emacs.d/snippets")
+(require 'yasnippet)
+(yas-global-mode 1)
+(defun my-yas/prompt (prompt choices &optional display-fn)
+  (let* ((names (loop for choice in choices
+                      collect (or (and display-fn (funcall display-fn choice))
+                                  choice)))
+         (selected (helm-other-buffer
+                    `(((name . ,(format "%s" prompt))
+                       (candidates . names)
+                       (action . (("Insert snippet" . (lambda (arg) arg))))))
+                    "*helm yas/prompt*")))
+    (if selected
+        (let ((n (position selected names :test 'equal)))
+          (nth n choices))
+      (signal 'quit "user quit!"))))
+(custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+(define-key yas-minor-mode-map (kbd "M-=") 'yas-insert-snippet)
+(setq yas/triggers-in-field t)
